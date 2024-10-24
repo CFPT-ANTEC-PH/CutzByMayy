22aa921283796dd438d06dd3acdf9899ae2bc5e7
simon-cdt <114390871+simon-cdt@users.noreply.github.com>
2024-10-25 01:20:48 +0200

    ios 2

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.cutzbymayy',
+  appName: 'cutzbymayy',
+  webDir: 'build'
+};
+
+export default config;
